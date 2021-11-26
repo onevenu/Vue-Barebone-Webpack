ff93814ba40d944f83d95c3d55355ccf8005891b
Vennu Gopal <ven.gop89@gmail.com>
2021-11-26 21:50:48 +0530

    Added HelloWorld comp

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
     {{ message }}
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   data() {
     return {
       message: "Hello World1",
     };
   },
+  name:'App',
+  components:{
+    HelloWorld
+  }
+
 };
 </script>
 
