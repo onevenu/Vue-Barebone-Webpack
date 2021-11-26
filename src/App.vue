a5645321fad89cb01709f9c229012bfb9343b664
Vennu Gopal <ven.gop89@gmail.com>
2021-11-26 21:53:45 +0530

    removed commented code and added style

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,11 @@ export default {
 
 <style>
 #app {
-  font-size: 18px;
-  font-family: "Roboto", sans-serif;
-  color: blue;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
